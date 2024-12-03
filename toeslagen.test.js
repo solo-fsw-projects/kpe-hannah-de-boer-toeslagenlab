e5dccfe23724471a96cea59ad68eb18a02ce9e0e
Chris de Jager <email@chrisdejager.nl>
2024-12-03 13:27:28 +0100

    fix manual testing version

--- toeslagen.test.js
+++ toeslagen.test.js
@@ -2,44 +2,48 @@
 const nextButton = document.getElementById("next-button");
 const description = document.getElementById("description");
 
+let monthState = 1;
+let currentMonthIndex = 0;
+
 nextButton.addEventListener("click", () =>
 {
-    if (currentMonthIndex >= totalMonths) {
-        description.textContent = "Simulation done.";
+    months = window.toeslagen.getMonths();
+    if (months === null) {
+        window.toeslagen.runAfterNextButton();
+        console.log("first time load months");
         return;
     }
     month = months[currentMonthIndex];
-
-    total = simulation.getTotal();
-    updateProgressBar();
-    updateAmount(previousTotal, total);
+    if (!month) {
+        console.log('Simulation is done.');
+        return;
+    }
+    window.toeslagen.runAfterNextButton(0, 526, month.name);
 
     switch (monthState) {
         case 1:
-            simulation.applyIncomes(month.getIncomes());
+            window.toeslagen.applyIncomes(month.getIncomes());
             description.innerHTML = "Je ontvangt inkomen van:<br><br>" + month.getIncomes().map(item => `${item.getName()}: ${item.getAmount()}`).join('<br>') + '.';
             monthState++;
             break;
         case 2:
-            simulation.applyFixedExpenses(month.getFixedExpenses());
+            window.toeslagen.applyFixedExpenses(month.getFixedExpenses());
             description.innerHTML = "Je vaste lasten moet je betalen:<br><br>" + month.getFixedExpenses().map(item => `${item.getName()}: ${item.getAmount()}`).join('<br>') + '.';
             monthState++;
             break;
         case 3:
-            let variableExpense = month.getNextVariableExpense();
+            let variableExpense = month.getNextVariableExpense(false);
             if (variableExpense === null) {
                 currentMonthIndex++;
                 monthState = 1;
                 description.textContent = "Geen variabele uitgaven meer deze maand.";
             } else {
-                simulation.applyExpense(variableExpense);
+                window.toeslagen.applyVariableExpense();
                 description.textContent = `${variableExpense.getDescription() ?? variableExpense.getName()} met een bedrag van ${variableExpense.getAmount()} euro.`;
             }
             break;
         case 4:
             console.log('monthState reached 4 not possible!');
     }
-
-    previousTotal = total;
 });
 
