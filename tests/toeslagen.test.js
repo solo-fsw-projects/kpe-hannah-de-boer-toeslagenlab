73c0c85adb3c3f3309ee1a22e46e11fcfc4f9296
Chris de Jager <email@chrisdejager.nl>
2025-01-16 13:19:35 +0100

    improve test

--- tests/toeslagen.test.js
+++ tests/toeslagen.test.js
@@ -7,9 +7,10 @@ require('../src/toeslagen');
 const fs = require('fs');
 
 describe('test loading of months data', () => {
+    const sheetCsv = fs.readFileSync('tests/sheet1.csv').toString();
+    window.fetch = mockFetch(sheetCsv);
+
     test('loads months data correctly', async () => {
-        const sheetCsv = fs.readFileSync('tests/sheet1.csv').toString();
-        window.fetch = mockFetch(sheetCsv);
         await window.toeslagen.runOnNewSlide('http://localhost', 1, 0);
         const months = window.toeslagen.getMonths();
         expect(months).not.toBeNull();
